3710f36d5f2b84689638d2e15d6cf2215177176d
David Calle <davidcalle9430@gmail.com>
2017-03-12 20:26:12 -0500

    worktype service

--- src/providers/wortktype-service.ts
+++ src/providers/wortktype-service.ts
@@ -1,8 +1,13 @@
 
 import { Injectable }    from '@angular/core';
 
-import  { WorkType } from '../models/worktype';
+import { Headers, Http, RequestOptions } from '@angular/http'
 
+import { WorkType } from '../models/worktype';
+import { SERVER_URL } from './services.util'
+
+import 'rxjs/Rx'
+import { Observable } from "rxjs/Observable";
 
 const WORK_TYPES: WorkType[] = [
     {id: 1, name: 'Carpintería', description: 'Profesionales 50,000 COP/hora', icon: 'images/carpinteria.png'},
@@ -13,12 +18,25 @@ const WORK_TYPES: WorkType[] = [
 
 @Injectable()
 export class WorkTypeService {
-    constructor(){
 
+    constructor(private http: Http){    }
+
+    getWorkTypes(): Observable<WorkType[]> {
+        var token = localStorage.getItem('token')
+        let worktTypesUrl: string = `${SERVER_URL}/api/worktypes/`;
+        var headers = new Headers({ 
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': `Token ${token}`
+        });
+        var options = new RequestOptions({ headers: headers });
+        return this.http.get(worktTypesUrl, options)
+                        .map(response => response.json() as WorkType[])
+                        .catch(this.handleError)
     }
 
-
-    getWorkTypes(): Promise<WorkType[]> {
-        return Promise.resolve(WORK_TYPES)
+    private handleError(error: any): Observable<any> {
+        console.error('An error occurred', error);
+        return Observable.throw(error.message || error);
     }
 }
