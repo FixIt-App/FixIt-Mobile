0b14359a5d8d92cf214f9fa1b645e6b8c130f2b2
David Calle <davidcalle9430@gmail.com>
2017-03-18 10:04:44 -0500

    rollback server location

--- src/providers/services.util.ts
+++ src/providers/services.util.ts
@@ -3,5 +3,5 @@ const SERVER_LOCATION: string = "192.168.0.11";
 
 const SERVER_PORT: string = "8000"
 
-export const SERVER_URL = "http://" + SERVER_LOCATION + ":" + SERVER_PORT
-//export const SERVER_URL = "";
+//export const SERVER_URL = "http://" + SERVER_LOCATION + ":" + SERVER_PORT
+export const SERVER_URL = "";
