105b49b3bf270b12032e241fb464e76200689990
Alfredo Santamaria <chino.sebastian@gmail.com>
2017-03-24 17:28:43 -0500

    geocoder working both ways this closes #5

--- src/pages/new-address/new-address.ts
+++ src/pages/new-address/new-address.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
-import { Geolocation } from 'ionic-native';
+import { Geolocation, NativeGeocoder } from 'ionic-native';
 
 declare var google;
 
@@ -11,7 +11,7 @@ declare var google;
 })
 export class NewAddressPage {
 
-  // trackTypes: any[] = [
+  // streetTypes: any[] = [
   //   { name: 'Avenida', abbreviation: 'AV' },
   //   { name: 'Avenida Calle', abbreviation: 'AV CLL' },
   //   { name: 'Avenida Carrera', abbreviation: 'AV CR' },
@@ -25,7 +25,17 @@ export class NewAddressPage {
   //   { name: 'Vía', abbreviation: 'VIA' }
   // ];
 
-  trackTypes: string[] = [
+  streetAbb: any = {
+    'Av' : 'Avenida',
+    'Ac': 'Avenida Calle',
+    'Ak': 'Avenida Carrera',
+    'Cl': 'Calle',
+    'Cra': 'Carrera',
+    'Ai': 'Circunvalar',
+    'Dg': 'Diagonal',
+    'Tv': 'Transversal',
+  }
+  streetTypes: string[] = [
     'Avenida',
     'Avenida Calle',
     'Avenida Carrera',
@@ -43,31 +53,46 @@ export class NewAddressPage {
   @ViewChild('locationIcon') locationIcon: ElementRef;
   map: any;
 
-  trackType: AbstractControl;
-  trackNumber: AbstractControl;
+  streetType: AbstractControl;
+  streetNumber: AbstractControl;
   genNumber: AbstractControl;
   placaNumber: AbstractControl;
+
+  // streetType: string;
+  // streetNumber: string;
+  // genNumber: string;
+  // placaNumber: string;
+
   form: FormGroup;
   mapLoaded: boolean;
+  geocoder: any;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private formBuilder: FormBuilder,
-              private viewCtrl: ViewController) 
+              private viewCtrl: ViewController,
+              private ngZone: NgZone)
   {
     
     this.form = this.formBuilder.group({
-        trackType:   ['', Validators.compose([Validators.required])],
-        trackNumber: ['', Validators.compose([Validators.required])],
+        streetType:   ['', Validators.compose([Validators.required])],
+        streetNumber: ['', Validators.compose([Validators.required])],
         genNumber:   ['', Validators.compose([Validators.required])],
         placaNumber: ['', Validators.compose([Validators.required])],
       });
-    this.form.patchValue({ 'trackType' : 'Calle'});
-    this.trackType = this.form.controls['trackType'];
-    this.trackNumber = this.form.controls['trackNumber'];
+    this.form.patchValue({ 'streetType' : 'Calle'});
+    this.streetType = this.form.controls['streetType'];
+    this.streetNumber = this.form.controls['streetNumber'];
     this.genNumber = this.form.controls['genNumber'];
     this.placaNumber = this.form.controls['placaNumber'];
+
+    // this.streetType = 'Calle';
     this.mapLoaded = false;
+    this.geocoder = new google.maps.Geocoder();
+    this.form.valueChanges
+             .debounceTime(600)
+             .distinctUntilChanged()
+             .subscribe(() => this.getLocByAddres());
   }
 
   ionViewDidLoad() {
@@ -87,8 +112,15 @@ export class NewAddressPage {
         }
     
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        // 'center_changed'
+        
+        this.map.addListener('dragend', () => {
+          this.getCenterGeocode();
+        });
+
         this.mapLoaded = true;
         this.setMarker();
+        this.getCenterGeocode();
       }, 
       (err) => {
         console.log(err);
@@ -108,47 +140,70 @@ export class NewAddressPage {
     this.locationIcon.nativeElement.style.left = (divWidth - (iconWidth/2)) +  "px";
   }
   
-  addMarker() {
- 
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
-    });
-  
-    let content = "<h4>Information!</h4>";          
-  
-    this.addInfoWindow(marker, content);
+  getCenterGeocode() {
     let pos = this.map.getCenter();
     console.log(pos.lat);
     console.log(pos.lng);
-    this.getGeoLocation(pos.lat(), pos.lng());
+    this.getGeocode(pos.lat(), pos.lng());
   
   }
 
-  getGeoLocation(lat: number, lng: number) {
+  getLocByAddres() {
+    let address: string = `${this.streetType.value} ${this.streetNumber.value} ${this.genNumber.value}-${this.placaNumber.value}, Bogotá, Colombia`;
+    console.log(address);
+    let geocoder = new google.maps.Geocoder();
+     this.geocoder.geocode({address: address},
+      (results, status) => {
+        console.log(status);
+        console.log(results);
+        if (status == google.maps.GeocoderStatus.OK && results && results.length > 0) {
+          console.log(results[0].geometry.location);
+          this.map.setCenter(results[0].geometry.location);
+        }
+      }
+     );
+  }
+  getGeocode(lat: number, lng: number) {
     if (navigator.geolocation) {
-        let geocoder = new google.maps.Geocoder();
         let latlng = new google.maps.LatLng(lat, lng);
         let request = { latLng: latlng };
-        console.log('voy a llamar geolocation');
-        console.log(lat + ' ' + lng);
-        geocoder.geocode(request, 
-        (results, status) => {
-          if (status == google.maps.GeocoderStatus.OK) {
-            let result = results[0];
-            console.log(result);
-            let rsltAdrComponent = result.address_components;
-            let resultLength = rsltAdrComponent.length;
-            if (result != null) {
-              console.log(rsltAdrComponent);
-              console.log(rsltAdrComponent[0].long_name);
-              console.log(rsltAdrComponent[1].long_name);
-            } else {
-              alert("No address available!");
+        this.geocoder.geocode(request,
+          (results, status) => {
+            if (status == google.maps.GeocoderStatus.OK) {
+              let result = results[0];
+              console.log(result);
+              let rsltAdrComponent = result.address_components;
+              let resultLength = rsltAdrComponent.length;
+              if (result != null) {
+                let street: string[] = rsltAdrComponent[1].short_name.split(/[ \.]+/);
+                let streetName: string = street[0];
+                let streetNumber: string = street.slice(1,street.length).join(" ");
+                let genNumbers: string[] = rsltAdrComponent[0].long_name.split('-');
+                let formated: string = result.formatted_address;
+                if(formated.indexOf('#') != -1) {
+                  let genNumbers2: string = formated.split('#')[1].split(' ')[0];
+                  console.log(genNumbers2);
+                  genNumbers = genNumbers2.split(/[-,]+/);
+                } else {
+                  let genNumbers = ["", ""];
+                }
+                /**
+                 * force anglar to rerender values
+                 * this has to be done since map listener is not registered
+                 */
+                if(this.streetType.value != this.streetAbb[streetName])
+                  this.ngZone.run(() => this.form.patchValue({ 'streetType' : this.streetAbb[streetName]}));
+                if(this.streetNumber.value != streetNumber)
+                  this.ngZone.run(() => this.form.patchValue({ 'streetNumber': streetNumber}));
+                if(this.genNumber.value != genNumbers[0])
+                  this.ngZone.run(() => this.form.patchValue({ 'genNumber': genNumbers[0]}));
+                if(this.placaNumber.value != genNumbers[1])
+                  this.ngZone.run(() => this.form.patchValue({ 'placaNumber': genNumbers[1]}));
+              } else {
+                alert("No address available!");
+              }
             }
-          }
-        });
+          });
     }
   }
 
@@ -157,7 +212,6 @@ export class NewAddressPage {
     let infoWindow = new google.maps.InfoWindow({
       content: content
     });
-  
     google.maps.event.addListener(marker, 'click', () => {
       infoWindow.open(this.map, marker);
     });
@@ -173,7 +227,7 @@ export class NewAddressPage {
     }
   }
 
-   dismiss() {
+  dismiss() {
     this.viewCtrl.dismiss();
   }
 
