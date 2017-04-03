54c2080a65e95905d08644a7707736bcb28d431c
David Calle <davidcalle9430@gmail.com>
2017-04-02 20:54:10 -0500

    updated remote server ip

--- src/providers/services.util.ts
+++ src/providers/services.util.ts
@@ -1,7 +1,7 @@
 
-const SERVER_LOCATION: string = "http://192.168.0.3";
+const SERVER_LOCATION: string = "http://104.131.151.33";
 
 const SERVER_PORT: string = "8000";
 
-// export const SERVER_URL = SERVER_LOCATION + ":" + SERVER_PORT;
-export const SERVER_URL = "";
+export const SERVER_URL = SERVER_LOCATION + ":" + SERVER_PORT;
+//export const SERVER_URL = "";
