dacd1a20b80ac55d4c3c1fff2c94d68568b03d22
Alfredo Santamaria <chino.sebastian@gmail.com>
2017-04-03 13:11:42 -0500

    changing where and what style

--- src/pages/what/what.ts
+++ src/pages/what/what.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Camera } from 'ionic-native';
+
+import { WorkService } from '../../providers/work-service';
 
 @Component({
   selector: 'page-what',
@@ -8,13 +11,30 @@ import { Camera } from 'ionic-native';
 })
 export class WhatPage {
 
-  images: string[];
+  images: { src: string, 
+            isUploading: boolean, 
+            idServer: number,
+            id: number }[];
+  currId: number;
+  textExplanation: AbstractControl;
+  currentMarker: any;
 
-  constructor(public navCtrl: NavController, 
+  form: FormGroup;
+  submitAttempt: boolean;
+
+  constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public alertCtrl: AlertController) 
+              public alertCtrl: AlertController,
+              public toastCtrl: ToastController,
+              private formBuilder: FormBuilder,
+              private workService: WorkService) 
   {
     this.images = [];
+    this.currId = 0;
+    this.form = this.formBuilder.group({
+      textExplanation:   ['', Validators.compose([Validators.required])],
+    });
+    this.textExplanation = this.form.controls['textExplanation'];
   }
 
   ionViewDidLoad() {
@@ -25,8 +45,9 @@ export class WhatPage {
     // console.log('entre a al camar');
     let options = {
         quality: 100,
-        destinationType: Camera.DestinationType.FILE_URI,
+        destinationType: Camera.DestinationType.DATA_URL,
         sourceType: Camera.PictureSourceType.CAMERA,
+        encodingType: Camera.EncodingType.PNG,
         targetWidth: 1000,
         targetHeight: 1000,
         correctOrientation: true,
@@ -38,16 +59,49 @@ export class WhatPage {
       options.correctOrientation = false;
     }
 
+    let id = this.currId++;
+    this.sleep(700).then(() => {
+      this.images.push({ src: '', isUploading: true, idServer: -1, id: id});
+    })
+
     Camera.getPicture(options).then(
       (imageURI) => {
-        console.log(imageURI);
-        this.images.push(imageURI);
+        console.log('voy a llamar');
+        console.log(this.images.length);
+        this.workService.sendImage(imageURI).subscribe(
+          (data) => {
+            console.log(data.id);
+            for(let image of this.images) {
+              if(image.id == id) {
+                this.images[this.images.length-1].src = `data:image/png;base64,${imageURI}`;
+                this.images[this.images.length-1].idServer = data.id;
+                this.images[this.images.length-1].isUploading = false;
+                break;
+              }
+            }
+          },
+          (error) => {
+            this.deleteImageNoConfirm(id);
+            //TODO what to do on error
+            console.log(error);
+          }
+        )
       }, (err) => {
+        this.deleteImageNoConfirm(id);
         console.log('error: ' + err);
       });
     }
 
-    deleteImage(index: number) {
+    deleteImageNoConfirm(id: number) {
+      for(let i = 0; i < this.images.length; i++) {
+        if(this.images[i].id == id) {
+          this.images.splice(i, 1);
+          break;
+        }
+      }
+    }
+
+    deleteImage(id: number) {
     let confirm = this.alertCtrl.create({
         title: '¿Eliminar imagen?',
         buttons: [
@@ -57,7 +111,7 @@ export class WhatPage {
           {
             text: 'Aceptar',
             handler: () => {
-              this.images.splice(index, 1);
+              this.deleteImageNoConfirm(id);
             }
           }
         ]
@@ -66,6 +120,22 @@ export class WhatPage {
   }
 
   finalize() {
-    
+    this.submitAttempt = true;
+    if(!this.form.valid) return;
+    if(this.images.filter( image => image.isUploading == true).length > 0) {
+      let toast = this.toastCtrl.create({
+        message: 'Por favor esperar a que cargen todas la imágens',
+        duration: 3000,
+        showCloseButton: true,
+        closeButtonText: 'Cerrar'
+      });
+      toast.present();
+    } else {
+      
+    }
   }
+
+  sleep (time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    }
 }
